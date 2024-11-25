7a8eab3da8e623eb838c2d4493f69c28c425a4db
Keleson <153941406+Keleson@users.noreply.github.com>
2024-11-25 00:01:30 -0300

    Cenario_projeto
    projeto base

--- Cenario_projeto/Assets/Script/MenuPrincipalManager.cs
+++ Cenario_projeto/Assets/Script/MenuPrincipalManager.cs
@@ -10,6 +10,8 @@ public class MenuPrincipalManager : MonoBehaviour
     [SerializeField] private string nomeDoLevel;
     [SerializeField] private GameObject painelMenuConfig; 
     [SerializeField] private GameObject painelMenuVolume; 
+    [SerializeField] private GameObject painelMenuConfirmar; 
+    [SerializeField] private GameObject painelMenuResolution; 
 
     public void Jogar()
     {
@@ -34,6 +36,16 @@ public class MenuPrincipalManager : MonoBehaviour
         }
     }
 
+    public void AbrirResolution()
+    {
+        painelMenuResolution.SetActive(true);
+    }
+
+    public void FecharResolution()
+    {
+        painelMenuResolution.SetActive(false);
+    }
+
      public void FecharOpcoes()
     {
         painelMenuConfig.SetActive(false); 
@@ -46,6 +58,22 @@ public class MenuPrincipalManager : MonoBehaviour
 
     public void Sair()
     {
+        painelMenuConfirmar.SetActive(true);
+    }
 
+    public void FecharConfirmacao()
+    {
+        painelMenuConfirmar.SetActive(false);
+    }
+
+    public void FecharJogo()
+    {
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 }
